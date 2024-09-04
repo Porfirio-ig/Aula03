c8a74b7742fc0148ad26621ae214b766c0feb6be
Igor Porfirio <igor.porfirioo@gmail.com>
2024-09-03 22:51:56 -0300

    Implementando operador OU

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class OperadorE {
 
 	public static void main(String[] args) {
-		/*Faça um programa que verifique se o usuário digitado é igual a 'administrador' e se a senha digitada é igual a '123'*/
+		/*Faï¿½a um programa que verifique se o usuï¿½rio digitado ï¿½ igual a 'administrador' e se a senha digitada ï¿½ igual a '123'*/
 		Scanner leitor = new Scanner(System.in);
 		String usuario, senha;
 		System.out.println("SISTEMA DE LOGIN");
@@ -11,15 +11,18 @@ public class OperadorE {
 		usuario = leitor.next();
 		System.out.println("Por favor, digite sua senha:");
 		senha = leitor.next();	
-		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
-		
-	if (usuario.equalsIgnoreCase("administrador") && senha.equals("123")){
-	System.out.println("login bem sucedido");
-	} else {
-		System.out.println("Usuarioce/ou senha incorretos");
-	}
-		leitor.close();
+		//Aqui ï¿½ preciso criar um if que verifique as duas condiï¿½ï¿½es para o login ser bem sucedido
 
+		if(usuario.equals("Administrador") && senha.equals("123")) {
+			
+			System.out.println("Login bem sucedido");
+		}else {
+			
+			System.out.println("Usuario e/ou senha incorretos");
+		}
+		
+		leitor.close();
+		
 	}
 
 }
